4d023ff2c0e8766a0db32654bfd3bae09719532b
Hieu Ly <hieubaly@hotmail.com>
2024-02-15 11:50:38 +1000

    subset data to planigale

--- master_code.R
+++ master_code.R
@@ -3,9 +3,9 @@
 library(tidyverse)
 library(boilrdata)
 
-marsupialsTibble <- as_tibble(marsupials)
+marsupialsTibble <- as_tibble(marsupials) %>% filter(Genus == "Planigale")
 
-summary(lm(MaxLifeSpan ~ Mass, data = marsupialsTibble))
+summary(lm(MaxLifeSpan ~ Mass, data = marsupialsTibble)) 
 
 daniels_Data <- read_csv("./data/ecotype-OB.csv")
 ggplot(marsupialsTibble, aes(x = Mass, y = MaxLifeSpan)) +
