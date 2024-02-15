d4a85cdbc4ba2656e5965cd8aebfdec5b2f3baa7
Hieu Ly <hieubaly@hotmail.com>
2024-02-15 11:54:43 +1000

    subset data to acrobates

--- master_code.R
+++ master_code.R
@@ -2,8 +2,9 @@
 
 library(tidyverse)
 library(boilrdata)
+View(marsupials)
 
-marsupialsTibble <- as_tibble(marsupials)
+marsupialsTibble <- as_tibble(marsupials) %>% filter(Genus == "Acrobates")
 
 summary(lm(MaxLifeSpan ~ Mass, data = marsupialsTibble))
 
